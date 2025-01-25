322aadb57d013e532621b05e78483df3e82b44c8
aancyboscoj <aancyboscoj@gmail.com>
2025-01-25 13:49:11 +0530

    again cors

--- src/main/java/com/fitnsport/server/Configuration/WebConfig.java
+++ src/main/java/com/fitnsport/server/Configuration/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
