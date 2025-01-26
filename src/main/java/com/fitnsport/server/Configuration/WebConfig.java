6f56ef56ce376ef9c121a5e016c04cb3be67ad5c
aancyboscoj <aancyboscoj@gmail.com>
2025-01-26 10:41:47 +0530

    testing cors

--- src/main/java/com/fitnsport/server/Configuration/WebConfig.java
+++ src/main/java/com/fitnsport/server/Configuration/WebConfig.java
@@ -2,20 +2,21 @@ package com.fitnsport.server.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Component
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
-//                .exposedHeaders("Access-Control-Allow-Origin");
-//                .allowCredentials(true);
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
